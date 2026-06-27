412cb300f2b2b10e56974f471b6a43f3f019a511
ADARSH SINGH <adarsh.siingh17@gmail.com>
2026-06-27 15:58:31 +0530

    Time: 5 ms (83.88%), Space: 46.7 MB (80.96%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int cr, int[][] pre) {
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0 ; i< cr ; i++ )
+        adj.add(new ArrayList<>());
+
+        int in[] = new int[cr] , top[] = new int[cr] ; int c = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int p[] : pre)
+        {
+            adj.get(p[1]).add(p[0]);
+            in[p[0]]++;
+        }
+
+        for (int i = 0; i < cr; i++) {
+        if (in[i] == 0)
+        q.add(i);
+        }
+        int j = 0;
+        while(!q.isEmpty())
+        {
+            int n = q.remove();
+            top[j++] = n;
+            c++;
+
+            for(int i : adj.get(n))
+            {
+                in[i]--;
+
+                if(in[i]==0)
+                q.add(i);
+            }
+        }
+
+        return (c==cr? top : (new int[0]));
+
+        
+    }
+}
