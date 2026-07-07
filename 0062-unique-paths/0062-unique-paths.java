61ad7fd0628a705c0ede74a8109fe87637e2e660
ADARSH SINGH <adarsh.siingh17@gmail.com>
2026-07-07 11:34:35 +0530

    Time: 1 ms (14.53%), Space: 42.2 MB (28.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -9,12 +9,19 @@ class Solution {
     }
     int count(int m , int n , int dp[][])
     {
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
+        dp[0][0] = 1;
 
-        if(dp[m][n] != -1) return dp[m][n];
+        for(int i = 0;i<m+1;i++)
+        for(int j = 0;j<n+1;j++)
+        {
+            if(i == 0 && j ==0 ) continue;
 
-        dp[m][n] = count(m-1,n,dp) + count(m,n-1,dp);
+            int up = i>0 ? dp[i-1][j] : 0;
+            int left = j>0 ? dp[i][j-1] : 0;
+
+            dp[i][j] = up + left;
+
+        }
 
         return dp[m][n];
     }
